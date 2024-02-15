e11a7168c1be90c009637990199072c8d22902a6
vinifred1003 <viniciusfrederico1003@gmail.com>
2024-02-15 20:32:47 -0300

    Function addTransaction was created

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class TransactionForm extends StatelessWidget {
-  TransactionForm({super.key});
-
   final titleController = TextEditingController();
   final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
 
   @override
   Widget build(BuildContext context) {
@@ -30,14 +32,15 @@ class TransactionForm extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 TextButton(
-                  onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
-                  },
                   child: const Text(
                     'Nova Transação',
                     style: TextStyle(color: Colors.purple),
                   ),
+                  onPressed: () {
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0;
+                    onSubmit(title, value);
+                  },
                 ),
               ],
             )
--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,9 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'transaction_form.dart';
 import 'transaction_list.dart';
 import '../models/transaction.dart';
 
 class TransactionUser extends StatefulWidget {
+  const TransactionUser({Key? key}) : super(key: key);
   @override
   State<TransactionUser> createState() => _TransactionUser();
 }
@@ -23,13 +26,24 @@ class _TransactionUser extends State<TransactionUser> {
       date: DateTime.now(),
     ),
   ];
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
+      children: [
         TransactionList(_transactions),
-        TransactionForm(),
+        TransactionForm(_addTransaction),
       ],
     );
   }
